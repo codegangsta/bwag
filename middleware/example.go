8db929b11852fa4965081b40bcc193426aae5eec
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-10-25 08:36:52 -0700

    Added example for Middleware

---
+++ middleware/example.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+)
+
+func main() {
+	// Middleware stack
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.HandlerFunc(MyMiddleware),
+		negroni.NewLogger(),
+		negroni.NewStatic(http.Dir("public")),
+	)
+
+	n.Run(":8080")
+}
+
+func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	log.Println("Logging on the way there...")
+
+	if r.URL.Query().Get("password") == "secret123" {
+		next(rw, r)
+	} else {
+		http.Error(rw, "Not Authorized", 401)
+	}
+
+	log.Println("Logging on the way back...")
+}
