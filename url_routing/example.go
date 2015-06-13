b35f9f2392a53063b2cee6e73902a4036799725e
Jeremy Saenz <jeremy.saenz@gmail.com>
2015-06-13 13:53:26 -0700

    Edited the router example to use httprouter instead of gorilla mux

--- url_routing/example.go
+++ url_routing/example.go
@@ -4,54 +4,51 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	r := mux.NewRouter().StrictSlash(false)
-	r.HandleFunc("/", HomeHandler)
+	r := httprouter.New()
+	r.GET("/", HomeHandler)
 
 	// Posts collection
-	posts := r.Path("/posts").Subrouter()
-	posts.Methods("GET").HandlerFunc(PostsIndexHandler)
-	posts.Methods("POST").HandlerFunc(PostsCreateHandler)
+	r.GET("/posts", PostsIndexHandler)
+	r.POST("/posts", PostsCreateHandler)
 
 	// Posts singular
-	post := r.PathPrefix("/posts/{id}").Subrouter()
-	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
-	post.Methods("GET").HandlerFunc(PostShowHandler)
-	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
-	post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
+	r.GET("/posts/:id", PostShowHandler)
+	r.PUT("/posts/:id", PostUpdateHandler)
+	r.GET("/posts/:id/edit", PostEditHandler)
 
 	fmt.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", r)
 }
 
-func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(rw, "Home")
 }
 
-func PostsIndexHandler(rw http.ResponseWriter, r *http.Request) {
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(rw, "posts index")
 }
 
-func PostsCreateHandler(rw http.ResponseWriter, r *http.Request) {
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(rw, "posts create")
 }
 
-func PostShowHandler(rw http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
+func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
 	fmt.Fprintln(rw, "showing post", id)
 }
 
-func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(rw, "post update")
 }
 
-func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(rw, "post delete")
 }
 
-func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
+func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(rw, "post edit")
 }
